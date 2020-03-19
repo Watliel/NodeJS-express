47ba5f70b839c8221e94a03de23c82229f8e763f
arnaud.salomon20@gmail.com <arnaud.salomon20@gmail.com>
2020-03-19 15:32:50 +0100

    ajout de la saisi de ville fonctionnel en GET

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ville', function(req, res, next) {
+  var param = req.query.ville
+  res.render('ville', { title: 'Les villes que vous voulez', ville: param });
+});
+
 module.exports = router;
