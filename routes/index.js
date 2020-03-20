f279ed53368cd90b0f91a410e1ff2534f3c47986
arnaud.salomon20@gmail.com <arnaud.salomon20@gmail.com>
2020-03-20 11:35:37 +0100

    Affichage des coordonnées des villes saisi

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
+var https = require('https');
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/ville', function(req, res, next) {
-  var param = req.body.ville
-  res.render('ville', { title: 'Les villes que vous voulez', ville: param });
+function requestCityData(cityName) {
+  return new Promise((resolve, reject) => {
+    https.get(`https://geocode.xyz/${cityName.replace(/ /g, '+')}?json=1`, (res) => {
+      res.on('data', (buffer) => {
+        resolve(JSON.parse(buffer.toString('utf8')));
+      });  
+    }).on('error', (e) => {
+      reject(e);
+    });
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Node App' });
 });
 
+router.post('/ville', async function(req, res, next) {
+  const data = await requestCityData(req.body.ville);
+  const longt = Number(data.longt);
+  const latt = Number(data.latt);
+  res.render('ville', {
+    ville: req.body.ville,
+    missingCoords: isNaN(longt) || longt === 0,
+    longt: longt,
+    latt: latt
+  });
+});
 module.exports = router;
